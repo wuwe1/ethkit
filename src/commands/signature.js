924d26f3b874eec44d3bf2f41cee7a8bd59a861f
wuwe1 <wu_we1@outlook.com>
2021-01-25 17:11:40 +0800

    ✨ signature: clean signature for input

--- src/commands/signature.js
+++ src/commands/signature.js
@@ -1,15 +1,35 @@
 const { ask } = require('../utils')
 
+const typeMap = new Map([['uint', 'uint256']])
+
+const strip = s => {
+  return s.replace(/^\s+|\s+$/g, '')
+}
+
+const clean = s => {
+  const functionName = s.match(/(\w+)\(.*\)/)[1]
+  let args = strip(s.match(/\((.*)\)/)[1])
+  const argTypes = args.split(',').map(arg => arg.split(' ')[0])
+  for (let i = 0; i < argTypes.length; i++) {
+    const t = argTypes[i]
+    argTypes[i] = typeMap.get(t) || t
+  }
+  return `${functionName}(${argTypes.join(',')})`
+}
+
 module.exports = {
   name: 'signature',
   alias: ['sig'],
   description: 'function signature',
-  run:async toolbox => {
+  run: async toolbox => {
     const { parameters, web3, print } = toolbox
     let arg = parameters.first
 
     if (arg === undefined) arg = await ask('arg')
-    const sig = web3.encodeFunctionSignature(arg)
-    print.info(sig)
+
+    const argCleaned = clean(arg)
+
+    const sig = web3.encodeFunctionSignature(argCleaned)
+    print.info(`${sig} <= ${argCleaned}`)
   }
 }
