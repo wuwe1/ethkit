f9dbf19bf60c9dfd51a7ab90367cf7d34adf00d8
wuwe1 <wu_we1@outlook.com>
2021-01-30 17:06:45 +0800

    ✨ add ERC20Info

--- src/commands/ERC20Info.js
+++ src/commands/ERC20Info.js
@@ -1,19 +1,60 @@
 const { ask } = require('../utils')
 
-const { TOTAL_SUPPLY } = require('../common/signature')
+const { NAME, SYMBOL, DECIMALS, TOTAL_SUPPLY } = require('../common/signature')
+
+const getTokenInfo = async (web3, token) => {
+  let symbol = await web3.eth.call({
+    to: token,
+    data: SYMBOL
+  })
+  symbol = web3.utils.hexToAscii(symbol)
+
+  let decimal = await web3.eth.call({
+    to: token,
+    data: DECIMALS
+  })
+  decimal = Number(decimal)
+
+  let name = await web3.eth.call({
+    to: token,
+    data: NAME
+  })
+  name = web3.utils.hexToAscii(name)
+
+  let totalSupply = await web3.eth.call({
+    to: token,
+    data: TOTAL_SUPPLY
+  })
+  totalSupply = web3.utils.hexToNumberString(totalSupply)
+
+  TOTAL_SUPPLY
+  return {
+    symbol,
+    name,
+    decimal,
+    totalSupply
+  }
+}
 
 module.exports = {
   name: 'ERC20Info',
   alias: ['info'],
-  description: 'get basic info of ERC20 token',
+  description: '<address> get basic info of ERC20 token',
   run: async toolbox => {
     const { parameters, web3, print } = toolbox
     let address = parameters.first
 
     if (address === undefined) address = await ask('address')
 
-    web3.eth.call()
+    const { symbol, name, decimal, totalSupply } = await getTokenInfo(
+      web3,
+      address
+    )
 
-    print.info(`${sig} <= ${argCleaned}`)
+    print.info(`symbol: ${symbol}`)
+    print.info(`name: ${name}`)
+    print.info(`decimal: ${decimal}`)
+    print.info(`totalSupply: ${totalSupply}`)
+    print.info(`totalSupply normalized: ${totalSupply / 10 ** decimal}`)
   }
 }
--- src/common/signature.js
+++ src/common/signature.js
@@ -1,15 +1,17 @@
-const GET_RESERVES = '0x0902f1ac'
+const NAME = '0x06fdde03'
 const TOKEN0 = '0x0dfe1681'
 const TOKEN1 = '0xd21220a7'
 const SYMBOL = '0x95d89b41'
 const DECIMALS = '0x313ce567'
 const TOTAL_SUPPLY = '0x18160ddd'
+const GET_RESERVES = '0x0902f1ac'
 
 module.exports = {
-    GET_RESERVES,
+    NAME,
     TOKEN0,
     TOKEN1,
     SYMBOL,
     DECIMALS,
-    TOTAL_SUPPLY
+    TOTAL_SUPPLY,
+    GET_RESERVES,
 }
